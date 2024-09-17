bf708992079a0cdec5c21b4f0440be8ee5714d54
Kunal Sunil Koli <kunalkoli537@gmail.com>
2024-09-17 10:30:58 +0530

    commit changes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-image': "url('https://t3.ftcdn.net/jpg/03/35/74/56/360_F_335745675_MaxYxSsadrviZdThITuHB2oCohYOiwEu.jpg')",
+      },
+    },
+  },
+  plugins: [],
+}
+
